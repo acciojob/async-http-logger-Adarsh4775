036c48234fdc342d984a01fc76d062e493e6ecdf
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-28 20:10:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+function httpLogger(){
+fetch("https://example.com/api/endpoint").then(response => response.json()).then(data => console.log("API Response:", data)).catch(error => console.error("Error:", error));
+}
+httpLogger();
